4f1d211ee42ec3fb9ed442dbf3e395ac7c97a220
Michael Stork <mstork@stream9.net>
2016-12-06 16:26:39 -0700

    api request validation

--- app/Http/Controllers/Api/SoapboxApiController.php
+++ app/Http/Controllers/Api/SoapboxApiController.php
@@ -16,14 +16,15 @@ class SoapboxApiController extends Controller
         $date = date('Y-m-d H:i:s', strtotime('now'));
 
         $validator = Validator::make($request->file(), [
-            'audioUpload' => 'mimetypes:audio/mp4,audio/mpeg,audio/ogg,audio/x-aac,audio/x-mpegurl,audio/x-wav'
+            'audioUpload' => 'bail|required|mimetypes:audio/mp4,audio/mpeg,audio/ogg,audio/x-aac,audio/x-mpegurl,audio/x-wav'
         ]);
 
         if ($validator->fails()) {
-            return response()->json(array_merge($validator->errors()->all(), ['extension' => $request->file('audioUpload')->extension()]));
+            return response()->json(['message' => $validator->errors()->all()[0]], 400);
         } else {
             $filename = $date . '.' . $request->file('audioUpload')->extension();
             $request->file('audioUpload')->move(base_path() . '/storage/app/public/', $filename);
+
             return response()->json([
                 'filename' => $filename,
                 'message' => 'Upload Complete'
@@ -33,19 +34,61 @@ class SoapboxApiController extends Controller
 
     public function validateNominee(Request $request)
     {
-        $email = $request->input('email');
+        $validator = Validator::make($request->input(), [
+            'email' => 'bail|required|email'
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->all()[0]], 400);
+        }
+
+        $email = $request->input('email');
+        
         try {
             $user = User::where('email', $email)->firstOrFail();
+
             return response()->json([
-                'available' => false,
-                'message' => 'Already Nominated!'
+                'message' => 'Already nominated!'
             ], 400);
+
         } catch (ModelNotFoundException $e) {
-            return response()->json([
-                'email' => $email,
-                'available' => true
-            ]);
+            return response()->json(['email' => $email]);
         }
     }
+
+    public function submission(Request $request)
+    {
+        $validator = $this->getSubmissionValidator($request);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => $validator->errors()->all()[0]], 400);
+        }
+
+        $this->saveSubmission($request->input());
+
+        return response()->json([
+            'message' => 'Thanks for participating!'
+        ], 200);
+    }
+
+    public function getSubmissionValidator(Request $request)
+    {
+        return Validator::make($request->input(), [
+           'auth.email' => 'bail|required|email',
+           'info.name' => 'required',
+           'info.title' => 'required',
+           'info.institution' => 'required',
+           'audio.filename' => 'required',
+           'nominees.submissionTitle' => 'required',
+           'nominees.nominee1' => 'bail|required|email|unique:users,email',
+           'nominees.nominee2' => 'bail|required|email|unique:users,email',
+           'nominees.nominee3' => 'bail|required|email|unique:users,email'
+        ]);
+    }
+
+    public function saveSubmission($input)
+    {
+            
+    }
+
 }
