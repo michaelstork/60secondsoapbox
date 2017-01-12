5adc334c3442964118200ba007c53cbb7bd5dc26
Michael Stork <mstork@stream9.net>
2017-01-11 20:35:15 -0700

    don't run browsersync in prod build script

--- gulpfile.js
+++ gulpfile.js
@@ -24,14 +24,16 @@ elixir(mix => {
                 }]
             }
         }
-    ).browserSync({
-        proxy: {
-            target: 'https://soapbox.localhost'
-        }
-    });
+    );
 
     if (inProduction) {
         mix.copy('node_modules/mdi/fonts', 'public/fonts')
         .copy('resources/assets/images', 'public/images');
+    } else {
+        mix.browserSync({
+            proxy: {
+                target: 'https://soapbox.localhost'
+            }
+        });
     }
 });
