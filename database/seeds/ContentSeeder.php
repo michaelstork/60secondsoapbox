1101c51a5980af9ad4262213a5478f9161130db2
Michael Stork <mstork@stream9.net>
2017-01-25 14:01:41 -0700

    added title column to content table

--- database/seeds/ContentSeeder.php
+++ database/seeds/ContentSeeder.php
@@ -14,6 +14,7 @@ class ContentSeeder extends Seeder
     {
         $intro = new Content();
         $intro->name = 'auth';
+        $intro->title = 'Intro Text';
         $intro->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!
 
 If you'd like to participate, enter your invitation code below!";
@@ -21,11 +22,13 @@ If you'd like to participate, enter your invitation code below!";
 
         $email = new Content();
         $email->name = 'email';
+        $email->title = 'Email Text';
         $email->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!";
         $email->save();
 
         $email = new Content();
         $email->name = 'audio';
+        $email->title = 'Audio Submission Instructions';
         $email->content = "If your device has a microphone and supports audio capture, you can record your submission here. Alternatively, you may upload your own audio file in wav or mp3 format.";
         $email->save();
     }
