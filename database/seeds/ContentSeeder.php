6f108d6c46843bf5e1d5df67f0abd21d369b33d7
Michael Stork <mstork@stream9.net>
2017-01-25 12:56:04 -0700

    content seeder

--- database/seeds/ContentSeeder.php
+++ database/seeds/ContentSeeder.php
@@ -13,13 +13,20 @@ class ContentSeeder extends Seeder
     public function run()
     {
         $intro = new Content();
-        $intro->name = 'intro';
-        $intro->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!";
+        $intro->name = 'auth';
+        $intro->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!
+
+If you'd like to participate, enter your invitation code below!";
         $intro->save();
 
         $email = new Content();
         $email->name = 'email';
         $email->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!";
+        $email->save();
+
+        $email = new Content();
+        $email->name = 'audio';
+        $email->content = "If your device has a microphone and supports audio capture, you can record your submission here. Alternatively, you may upload your own audio file in wav or mp3 format.";
         $email->save();
     }
 }
