50bf841887cba4167b7723c0af7e5d65397a80fd
Michael Stork <mstork@stream9.net>
2017-01-30 14:42:27 -0700

    content default values

--- database/seeds/ContentSeeder.php
+++ database/seeds/ContentSeeder.php
@@ -23,14 +23,14 @@ If you'd like to participate, enter your invitation code below!";
 
         $subject = new Content();
         $subject->name = 'email-subject';
-        $subject->title = 'Email Subject';
+        $subject->title = 'Invitation Email Subject';
         $subject->content = "You've been invited to record an episode of 60 Second Soapbox!";
         $subject->type = 'text';
         $subject->save();
 
         $email = new Content();
         $email->name = 'email-body';
-        $email->title = 'Email Text';
+        $email->title = 'Invitation Email Body';
         $email->content = "Any topic is on the table – clinical, academic, economic, or whatever else may interest an EM-centric audience. We carefully remix your audio to add an extra splash of drama and excitement. Even more exciting, you'll get to challenge 3 of your peers to stand on a soapbox of their own!";
         $email->type = 'textarea';
         $email->save();
